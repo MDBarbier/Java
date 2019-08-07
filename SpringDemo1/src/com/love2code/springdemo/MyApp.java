be1a4acb8f85ab6826669e1e5b1e0a0e7c506ad6
matt barbier <matthewdavidbarbier@gmail.com>
2019-08-07 21:58:11 +0100

    initial commit of spring demo 1

---
+++ SpringDemo1/src/com/love2code/springdemo/MyApp.java
@@ -0,0 +1,12 @@
+package com.love2code.springdemo;
+
+public class MyApp {
+
+	public static void main(String[] args) {
+		
+		Coach theCoach = new BaseballCoach();
+		
+		System.out.println(theCoach.getDailyWorkout());
+	}
+
+}
