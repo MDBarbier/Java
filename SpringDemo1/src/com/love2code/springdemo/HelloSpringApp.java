5db58cd86a56053232745bd62f360768014a6963
Matt Barbier <matthewdavidbarbier@gmail.com>
2019-11-02 20:07:18 +0000

    updated spring demo to actually use Spring and to use IOC

---
+++ SpringDemo1/src/com/love2code/springdemo/HelloSpringApp.java
@@ -0,0 +1,23 @@
+package com.love2code.springdemo;
+
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class HelloSpringApp {
+
+	public static void main(String[] args) {
+
+		//Load spring configuration file ("Application context")
+		ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml"); 
+		
+		//Retrieve bean from container
+		Coach theCoach = context.getBean("myCoach", Coach.class);
+		
+		//Call methods on the bean
+		System.out.println(theCoach.getDailyWorkout());
+		
+		//Close the context
+		context.close();
+
+	}
+
+}
---
+++ SpringDemo1/src/com/love2code/springdemo/HockeyCoach.java
@@ -0,0 +1,12 @@
+package com.love2code.springdemo;
+
+public class HockeyCoach implements Coach
+{
+
+	@Override
+	public String getDailyWorkout() {
+		
+		return "Do twenty wrist-shots and twenty slapshots!";
+	}
+
+}
